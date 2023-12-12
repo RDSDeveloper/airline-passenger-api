c445d0dc5c90217c87bcc0e6bed2aa0eb4866c2a
Rodrigo Santos <couriersix@MacBook-Air-de-Rodrigo.local>
2023-12-12 15:44:21 -0300

    Airplane model tested

--- airplanes/tests.py
+++ airplanes/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Airplane
+from math import log
 
-# Create your tests here.
+
+class AirplaneModelTest(TestCase):
+    def setUp(self):
+        self.airplane = Airplane.objects.create(passenger_count=100)
+
+    def test_fuel_tank_capacity(self):
+        expected_capacity = 200 * self.airplane.id
+        self.assertEqual(self.airplane.fuel_tank_capacity, expected_capacity)
+
+    def test_fuel_consumption_per_minute(self):
+        expected_consumption = (
+            log(self.airplane.id) * 0.80 + self.airplane.passenger_count * 0.002
+        )
+        self.assertEqual(
+            self.airplane.fuel_consumption_per_minute, expected_consumption
+        )
