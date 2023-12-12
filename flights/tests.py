c606955aa9200285cf45d8bf97c79ae76724903c
Rodrigo Santos <couriersix@MacBook-Air-de-Rodrigo.local>
2023-12-12 16:40:24 -0300

    Flight model improved and teste OK

--- flights/tests.py
+++ flights/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.utils import timezone
 from airplanes.models import Airplane
 from .models import Flight
+from django.core.exceptions import ValidationError
 
 
 class FlightModelTest(TestCase):
@@ -18,3 +19,13 @@ class FlightModelTest(TestCase):
     def test_str(self):
         expected_str = f"Flight from {self.flight.origin} to {self.flight.destination} on {self.flight.departure_time}"
         self.assertEqual(str(self.flight), expected_str)
+
+    def test_departure_time_must_be_before_arrival_time(self):
+        with self.assertRaises(ValidationError):
+            Flight.objects.create(
+                departure_time=timezone.now(),
+                arrival_time=timezone.now() - timezone.timedelta(hours=2),
+                origin="New York",
+                destination="London",
+                airplane=self.airplane,
+            )
