21589eb54b4aaa47721b5580812100077ba6c7f8
Rodrigo Santos <couriersix@MacBook-Air-de-Rodrigo.local>
2023-12-12 15:32:53 -0300

    Settings.py now secure

--- django_project_core/settings.py
+++ django_project_core/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,16 +22,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-5=&=x*f8#3*=)szh2v=ha%-!2bqw6a4klva7qf_ke8$)8)@bxi"
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False),
+    SECRET_KEY=(str, get_random_secret_key().replace("$", r"\$")),
+    ALLOWED_HOSTS=(list, []),
+)
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# reading .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
-ALLOWED_HOSTS = []
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG = env("DEBUG")
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
-# Application definition
 
 # Application definition
 CORE_APPS = [
