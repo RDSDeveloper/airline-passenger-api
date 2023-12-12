31489fe1c1f487b817dfbc5629460528ceea7de9
Rodrigo Santos <couriersix@MacBook-Air-de-Rodrigo.local>
2023-12-12 16:37:36 -0300

    Departure - arrival time logic created, the first one must have a date before the second one

--- flights/views.py
+++ flights/views.py
@@ -2,8 +2,17 @@
 from rest_framework import viewsets
 from .models import Flight
 from .serializers import FlightSerializer
+from rest_framework import status
+from rest_framework.response import Response
+from django.core.exceptions import ValidationError
 
 
 class FlightViewSet(viewsets.ModelViewSet):
     queryset = Flight.objects.all()
     serializer_class = FlightSerializer
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super().create(request, *args, **kwargs)
+        except ValidationError as e:
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
