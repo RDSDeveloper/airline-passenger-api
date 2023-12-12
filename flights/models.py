22ac95462a7ad46d2526c3dd42811a23c389f5f4
Rodrigo Santos <couriersix@MacBook-Air-de-Rodrigo.local>
2023-12-12 16:34:37 -0300

    Flights and airplanes serializers fixed

--- flights/models.py
+++ flights/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from airplanes.models import Airplane
+from django.core.exceptions import ValidationError
 
 
 class Flight(models.Model):
@@ -8,6 +9,11 @@ class Flight(models.Model):
     origin = models.CharField(max_length=200)
     destination = models.CharField(max_length=200)
     airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE)
+    
+    def save(self, *args, **kwargs):
+        if self.departure_time >= self.arrival_time:
+            raise ValidationError("Departure time must be before arrival time")
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return (
--- flights/serializers.py
+++ flights/serializers.py
@@ -2,10 +2,20 @@
 from rest_framework import serializers
 from .models import Flight
 from airplanes.serializers import AirplaneSerializer
+from airplanes.models import Airplane
+
 
 class FlightSerializer(serializers.ModelSerializer):
-    airplane = AirplaneSerializer(read_only=True)
+    airplane_detail = AirplaneSerializer(source="airplane", read_only=True)
+    airplane = serializers.PrimaryKeyRelatedField(queryset=Airplane.objects.all())
 
     class Meta:
         model = Flight
-        fields = ["departure_time", "arrival_time", "origin", "destination", "airplane"]
+        fields = [
+            "departure_time",
+            "arrival_time",
+            "origin",
+            "destination",
+            "airplane",
+            "airplane_detail",
+        ]
