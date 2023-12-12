0e496c957131554f2abd9d63118296fa01fbf15a
Rodrigo Santos <couriersix@MacBook-Air-de-Rodrigo.local>
2023-12-12 16:56:31 -0300

    99% of coveraged achieved so far, flight viewset teste Ok

--- flights/tests.py
+++ flights/tests.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from airplanes.models import Airplane
 from .models import Flight
 from django.core.exceptions import ValidationError
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from rest_framework import status
 
 
 class FlightModelTest(TestCase):
@@ -29,3 +32,39 @@ class FlightModelTest(TestCase):
                 destination="London",
                 airplane=self.airplane,
             )
+
+
+class FlightViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.airplane = Airplane.objects.create(passenger_count=100)
+        self.flight = Flight.objects.create(
+            departure_time=timezone.now(),
+            arrival_time=timezone.now() + timezone.timedelta(hours=2),
+            origin="New York",
+            destination="London",
+            airplane=self.airplane,
+        )
+
+    def test_create_flight(self):
+        url = reverse("flight-list")
+        data = {
+            "departure_time": timezone.now(),
+            "arrival_time": timezone.now() + timezone.timedelta(hours=2),
+            "origin": "New York",
+            "destination": "London",
+            "airplane": self.airplane.id,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_flight_validation_error(self):
+        url = reverse("flight-list")
+        data = {
+            "departure_time": timezone.now(),
+            "arrival_time": timezone.now() - timezone.timedelta(hours=2),
+            "origin": "New York",
+            "destination": "London",
+            "airplane": self.airplane.id,
+        }
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
