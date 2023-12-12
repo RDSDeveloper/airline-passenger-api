d9eeff642b6fcb5f8e07b7a70cca449a2d556e06
Rodrigo Santos <couriersix@MacBook-Air-de-Rodrigo.local>
2023-12-12 16:18:32 -0300

    Flight model tested and migrations applied

--- flights/tests.py
+++ flights/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.utils import timezone
+from airplanes.models import Airplane
+from .models import Flight
 
-# Create your tests here.
+
+class FlightModelTest(TestCase):
+    def setUp(self):
+        self.airplane = Airplane.objects.create(passenger_count=100)
+        self.flight = Flight.objects.create(
+            departure_time=timezone.now(),
+            arrival_time=timezone.now() + timezone.timedelta(hours=2),
+            origin="New York",
+            destination="London",
+            airplane=self.airplane,
+        )
+
+    def test_str(self):
+        expected_str = f"Flight from {self.flight.origin} to {self.flight.destination} on {self.flight.departure_time}"
+        self.assertEqual(str(self.flight), expected_str)
