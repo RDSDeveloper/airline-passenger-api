a87074e0691d867ea91c7935c39a16b5f81a6f1d
Rodrigo Santos <couriersix@MacBook-Air-de-Rodrigo.local>
2023-12-12 16:25:29 -0300

    Flight serializer now works with AirplaneSerializer

--- django_project_core/urls.py
+++ django_project_core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
     path("airplanes/", include("airplanes.urls")),
+    path("flights/", include("flights.urls")),
 ]
--- flights/serializers.py
+++ flights/serializers.py
@@ -1,9 +1,11 @@
 # serializers.py
 from rest_framework import serializers
 from .models import Flight
-
+from airplanes.serializers import AirplaneSerializer
 
 class FlightSerializer(serializers.ModelSerializer):
+    airplane = AirplaneSerializer(read_only=True)
+
     class Meta:
         model = Flight
         fields = ["departure_time", "arrival_time", "origin", "destination", "airplane"]
