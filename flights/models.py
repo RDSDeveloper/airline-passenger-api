117cdbcbd87dbb31f964602ab2e2a4b266b8b188
Rodrigo Santos <couriersix@MacBook-Air-de-Rodrigo.local>
2023-12-12 16:16:31 -0300

    Flight model created

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from airplanes.models import Airplane
 
-# Create your models here.
+
+class Flight(models.Model):
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+    origin = models.CharField(max_length=200)
+    destination = models.CharField(max_length=200)
+    airplane = models.ForeignKey(Airplane, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (
+            f"Flight from {self.origin} to {self.destination} on {self.departure_time}"
+        )
